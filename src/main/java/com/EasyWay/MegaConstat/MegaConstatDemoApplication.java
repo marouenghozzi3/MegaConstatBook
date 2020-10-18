e76dcbc7b5df4b2e506eea9773b0f8965249e34e
marouenghozzi3 <marouenghozzi3@gmail.com>
2020-10-18 13:33:10 +0100

    First snapshot of MegaConstatDemo

---
+++ src/main/java/com/EasyWay/MegaConstat/MegaConstatDemoApplication.java
@@ -0,0 +1,13 @@
+package com.EasyWay.MegaConstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaConstatDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaConstatDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EasyWay/MegaConstat/MegaConstatDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EasyWay.MegaConstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaConstatDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
